3f7bf9a43568098f42c9d2b3d1251f32b6f21890
Antonio <aalvarez@stratio.com>
2019-11-22 15:26:58 +0100

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public scene: any;
+  public camera: any;
+  public renderer: any;
   title = 'AmazingThings';
+
+  ngOnInit() {
+
+  }
+
+  render() {
+    requestAnimationFrame( this.render );
+    this.renderer.render(this.scene, this.camera);
+  }
+
+
+  setUpScene() {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( this.renderer.domElement );
+
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material );
+    this.scene.add( cube );
+
+    this.camera.position.z = 5;
+  }
 }
